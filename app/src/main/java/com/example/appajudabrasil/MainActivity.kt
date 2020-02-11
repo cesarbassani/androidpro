cd42d9e1881ad03d8edad0d43f5c011281d8fbaa
cesarbassani_dev <cesar.bassani@viewt.com.br>
2020-02-10 21:41:57 -0400

    Fazer integracao com a API do Governo de detalhes das propostas

--- app/src/main/java/com/example/appajudabrasil/MainActivity.kt
+++ app/src/main/java/com/example/appajudabrasil/MainActivity.kt
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,18 +17,43 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var recyclerView: RecyclerView
     lateinit var adapter: ProposicoesAdapter
+    lateinit var requestQueue: RequestQueue
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        list.add(Proposicao(1, "", "PEC", 2019, "Ementa de teste!"))
-        list.add(Proposicao(2, "", "PEC", 2019, "Segunda Ementa de teste!"))
+        requestQueue = Volley.newRequestQueue(this)
 
         recyclerView = findViewById(R.id.rv_proposicoes)
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
         adapter = ProposicoesAdapter(list, this)
         recyclerView.adapter = adapter
+
+        load()
+    }
+
+    private fun load() {
+        val request = JsonObjectRequest(Request.Method.GET,
+            "https://dadosabertos.camara.leg.br/api/v2/proposicoes?ordem=ASC&ordenarPor=id",
+            null,
+            Response.Listener <JSONObject>{
+                list.clear()
+                val dados = it.getJSONArray("dados")
+                for (i in 0 until dados.length()) {
+                    val item = dados.getJSONObject(i)
+                    list.add(Proposicao(item.getInt("id"),
+                        item.getString("uri"),
+                        item.getString("siglaTipo"),
+                        item.getInt("ano"),
+                        item.getString("ementa")))
+                }
+
+                adapter.notifyDataSetChanged()
+            },
+            null)
+
+        requestQueue.add(request)
     }
 }
